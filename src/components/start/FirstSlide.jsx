c34478f8820821f13c811d5ea09af847dd2593aa
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-05-04 11:10:41 +0200

    hemligt

---
+++ src/components/start/FirstSlide.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '..//..//styles/Start.css'
+
+const FirstSlide = () => {
+  return (
+    <div className="welcome-page">
+      <h2>Manero</h2>
+      <h4>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</h4>
+      <button>Nästa</button>
+    </div>
+  );
+}
+
+export default FirstSlide;
